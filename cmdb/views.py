0bbd58d5814ecd91c751da3d232d90730979c4b1
lvchengxin <275309914@qq.com>
2025-11-18 21:45:23 +0800

    feat(资产): 添加资产编辑功能
    - 在urls.py中添加edit路由
    - 在views.py中实现edit视图函数，支持资产编辑
    - 在main.html中添加编辑按钮和新增资产按钮
    - 修改add.html模板使其支持编辑和录入共用

--- cmdb/views.py
+++ cmdb/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render , HttpResponse,redirect
+from django.shortcuts import render , HttpResponse,redirect, get_object_or_404
 from .models import host
 from .forms import hostForm
 def index(request):
@@ -13,4 +13,15 @@ def add(request):
             return redirect('index')
     else:
         form = hostForm()
-    return render(request, 'add.html',{'form':form})
+    return render(request, 'add.html',{'form':form, 'title': '资产录入'})
+
+def edit(request, pk):
+    obj = get_object_or_404(host, pk=pk)
+    if request.method == 'POST':
+        form = hostForm(request.POST, instance=obj)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+    else:
+        form = hostForm(instance=obj)
+    return render(request, 'add.html', {'form': form, 'title': '资产编辑'})
